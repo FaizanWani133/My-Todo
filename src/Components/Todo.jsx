774eab9747fb3f9b678b9ebde19c46d5fa16b800
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2025-05-26 15:48:26 +0530

    Update Todo.jsx

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -56,8 +56,7 @@ return a.status -b.status
       ) : (
         <br></br>
       )}
-      {text === "Biku"  && <Heart /> }
-      {text === "biku"  && <Heart /> }
+      
       {text === "Faizan"  && <img width="300px" src='https://scontent.fluh1-1.fna.fbcdn.net/v/t1.6435-9/122590645_3356399684457658_3441755671862065481_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=iqMKCabLX0sAX-rAMs5&tn=kQJTYpj2vksXWzpP&_nc_ht=scontent.fluh1-1.fna&oh=00_AT_02D6yzIEfUJkrdo5CxTW2ZHJRIGEhbWhg4qdotG9AoQ&oe=634D070C' alt="faizan"/> }
     </div>
   );
